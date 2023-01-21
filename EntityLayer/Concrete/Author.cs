f158b3374f6d0f25a2b744bb143e94e7af79461b
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2023-01-21 17:46:47 +0300

    2023/01/21

---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
+
+        public string Mail { get; set; }
+        public string Password { get; set; }
+
+    }
+}
